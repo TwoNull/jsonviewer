d0c3dcc3913f70da3d66fdad5700544ab321e7ac
TwoNull <115444279+TwoNull@users.noreply.github.com>
2024-04-12 16:28:42 -0400

    Added arrays

--- app/components/viewer.tsx
+++ app/components/viewer.tsx
@@ -1,6 +1,6 @@
 import { Accordion, AccordionItem, Card, CardBody } from "@nextui-org/react";
-import { GoNoEntry } from "react-icons/go";
-import { VscSymbolBoolean, VscSymbolKey, VscSymbolNumeric } from "react-icons/vsc";
+import { GoNoEntry, GoQuestion } from "react-icons/go";
+import { VscJson, VscSymbolArray, VscSymbolBoolean, VscSymbolKey, VscSymbolNumeric } from "react-icons/vsc";
 
 
 const itemClasses = {
@@ -8,26 +8,45 @@ const itemClasses = {
 }
 
 export default function Viewer(props: {object: any}) {
-
     function renderAccordion(data: any) {
         return Object.entries(data).map(([key, value]) => {
             switch(typeof value) {
                 case "object":
-                    if (Object.is(value, null)) {
-                        return <span className="flex items-center gap-1 text-xs"><GoNoEntry />{key + ": " + String(value)}</span>
+                    const objectType = Object.prototype.toString.call(value)
+                    if (objectType === '[object Null]') {
+                        return <span className="flex items-center gap-1 text-xs"><GoNoEntry />{key + ":"}<span>null</span></span>
                     }
-                    return (
-                        <Accordion itemClasses={itemClasses} isCompact>
-                            <AccordionItem key={key} title={key}>
-                                <div className="flex flex-row gap-2">
-                                    <div className="border-[0.5px] border-neutral-400"/>
-                                    <div>
-                                        {renderAccordion(value)}
+                    if (objectType === '[object Undefined]') {
+                        return <span className="flex items-center gap-1 text-xs"><GoQuestion />{key + ":"}<span>undefined</span></span>
+                    }
+                    if (objectType === '[object Object]') {
+                        return (
+                            <Accordion itemClasses={itemClasses} isCompact>
+                                <AccordionItem key={key} title={key} startContent={<VscJson />}>
+                                    <div className="flex flex-row gap-2">
+                                        <div className="border-[0.5px] border-neutral-400"/>
+                                        <div>
+                                            {renderAccordion(value)}
+                                        </div>
                                     </div>
-                                </div>
-                            </AccordionItem>
-                        </Accordion>
-                    )
+                                </AccordionItem>
+                            </Accordion>
+                        )
+                    }
+                    if (objectType === '[object Array]') {
+                        return (
+                            <Accordion itemClasses={itemClasses} isCompact>
+                                <AccordionItem key={key} title={key} startContent={<VscSymbolArray />}>
+                                    <div className="flex flex-row gap-2">
+                                        <div className="border-[0.5px] border-neutral-400"/>
+                                        <div>
+                                            {renderAccordion(value)}
+                                        </div>
+                                    </div>
+                                </AccordionItem>
+                            </Accordion>
+                        )
+                    }
                 case "number":
                     return <span className="flex items-center gap-1 text-xs"><VscSymbolNumeric />{key + ": " + String(value)}</span>
                 case "string":
